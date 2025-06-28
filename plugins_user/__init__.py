0cd06e3e0c9cc45ac3817c6506bd24ba6bfdedb8
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2025-06-28 18:17:28 -0500

    Prepared for agentic work.

--- plugins_user/__init__.py
+++
@@ -1,9 +0,0 @@
-from plugins_user.plugins.fun_fact_tool import FunFactTool
-from plugins_user.plugins.memory_tools import MemorySearchTool, StoreMemoryTool
-
-
-PLUGIN_TOOLS = [
-    StoreMemoryTool,
-    MemorySearchTool,
-    FunFactTool,
-]
