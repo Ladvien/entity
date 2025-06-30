845f169ebf1c8adf00759aa1a44a5533b74d370e
Thomas Brittain <honeysucklehardware@gmail.com>
2025-06-30 17:50:56 -0500

    [Resource] Add OpenAI chat completion resource

--- src/pipeline/plugins/resources/__init__.py
+++ src/pipeline/plugins/resources/__init__.py
@@ -1,6 +1,7 @@
 from .echo_llm import EchoLLMResource
 from .memory_resource import SimpleMemoryResource
 from .ollama_llm import OllamaLLMResource
+from .openai import OpenAIResource
 from .postgres import PostgresResource
 from .structured_logging import StructuredLogging
 from .vector_memory import VectorMemoryResource
@@ -12,4 +13,5 @@ __all__ = [
     "StructuredLogging",
     "PostgresResource",
     "VectorMemoryResource",
+    "OpenAIResource",
 ]
