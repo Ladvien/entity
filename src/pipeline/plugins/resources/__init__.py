af85461b6a656219e91d23093dcdbed486a4bd20
Thomas Brittain <honeysucklehardware@gmail.com>
2025-07-01 18:36:56 -0500

    fix: resolve leftover merge conflicts

--- src/pipeline/plugins/resources/__init__.py
+++ src/pipeline/plugins/resources/__init__.py
@@ -8,15 +8,9 @@ from .memory_resource import SimpleMemoryResource
 from .memory_storage import MemoryStorage
 from .ollama_llm import OllamaLLMResource
 from .openai import OpenAIResource
-<<<<<<< HEAD
-from .postgres import (ConnectionPoolResource, PostgresPoolResource,
-                       PostgresResource)
-from .storage_backend import StorageBackend, StorageResource
-=======
 from .postgres import ConnectionPoolResource, PostgresPoolResource, PostgresResource
 from .sqlite_storage import SQLiteStorage
 from .storage_backend import StorageBackend
->>>>>>> 41e9ce33055fea658287c473f9fa43fe2b5cbcc7
 from .structured_logging import StructuredLogging
 from .vector_memory import VectorMemoryResource
 
@@ -26,8 +20,6 @@ __all__ = [
     "OllamaLLMResource",
     "SimpleMemoryResource",
     "StructuredLogging",
-    "StorageBackend",
-    "StorageResource",
     "ConnectionPoolResource",
     "PostgresPoolResource",
     "PostgresResource",
