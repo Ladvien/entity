630a2d4f7ff5f7800b77899509f0739c4b716a37
Thomas Brittain <honeysucklehardware@gmail.com>
2025-07-02 11:35:13 -0500

    [Resource] Add FileSystemResource abstraction

--- src/pipeline/resources/__init__.py
+++ src/pipeline/resources/__init__.py
@@ -1,5 +1,12 @@
 from .database import DatabaseResource
+from .filesystem import FileSystemResource
 from .llm import LLM, LLMResource
 from .memory import Memory
 
-__all__ = ["LLM", "LLMResource", "Memory", "DatabaseResource"]
+__all__ = [
+    "LLM",
+    "LLMResource",
+    "Memory",
+    "DatabaseResource",
+    "FileSystemResource",
+]
