0667d292328103375a6f25c96fb51e6638d5e820
Thomas Brittain <honeysucklehardware@gmail.com>
2025-07-04 09:31:27 -0500

    Move cache and sandbox modules to user_plugins

--- src/pipeline/cache/__init__.py
+++ src/pipeline/cache/__init__.py
@@ -1,8 +1,9 @@
 """Caching utilities with pluggable backends."""
 
+from user_plugins.resources.cache_backends.redis import RedisCache
+from user_plugins.resources.cache_backends.semantic import SemanticCache
+
 from .base import CacheBackend
 from .memory import InMemoryCache
-from .redis import RedisCache
-from .semantic import SemanticCache
 
 __all__ = ["CacheBackend", "InMemoryCache", "RedisCache", "SemanticCache"]
