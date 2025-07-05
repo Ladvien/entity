6c21f93ad9776b110bde5b0c7561011e560a64da
Thomas Brittain <honeysucklehardware@gmail.com>
2025-07-04 23:31:06 -0500

    Fix self-referential cache import

--- plugins/resources/cache.py
+++ plugins/resources/cache.py
@@ -1,3 +1,6 @@
-from plugins.resources.cache import CacheResource
+"""Compatibility shim for accessing :class:`CacheResource`."""
+
+# Import the implementation from the user_plugins package to avoid recursion.
+from pipeline.user_plugins.resources.cache import CacheResource
 
 __all__ = ["CacheResource"]
