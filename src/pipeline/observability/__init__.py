ef9f06b0bd6161c6c92426c999bf3e65fa4f03e1
Thomas Brittain <honeysucklehardware@gmail.com>
2025-07-05 18:54:18 -0500

    Add metrics server accessor

--- src/pipeline/observability/__init__.py
+++ src/pipeline/observability/__init__.py
@@ -1,13 +1,14 @@
 """Observability helpers for logging and metrics."""
 
 from .memory import profile_memory
-from .metrics import start_metrics_server
+from .metrics import get_metrics_server, start_metrics_server
 from .tracing import start_span, traced
 from .utils import execute_with_observability
 
 __all__ = [
     "execute_with_observability",
     "start_metrics_server",
+    "get_metrics_server",
     "start_span",
     "traced",
     "profile_memory",
