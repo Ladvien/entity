d58e4144bbfb321cfd930d3553986ee909147400
Thomas Brittain <honeysucklehardware@gmail.com>
2025-07-06 18:09:52 -0500

    Fix failure plugin imports

--- src/plugins/builtin/failure/error_formatter.py
+++ src/plugins/builtin/failure/error_formatter.py
@@ -1,5 +1,5 @@
-"""Public re-export of :class:`ErrorFormatter`."""
+"""Public re-export of :class:`ErrorFormatter` from :mod:`user_plugins`."""
 
-from plugins.builtin.failure.error_formatter import ErrorFormatter
+from user_plugins.failure.error_formatter import ErrorFormatter
 
 __all__ = ["ErrorFormatter"]
