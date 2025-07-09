0936d296b8d520e3feb30e750ea6a9e0a1934285
Thomas Brittain <honeysucklehardware@gmail.com>
2025-07-09 14:52:11 -0500

    Remove metrics server wrapper helpers

--- src/pipeline/observability/__init__.py
+++ src/pipeline/observability/__init__.py
@@ -1,16 +1,9 @@
-from .metrics import (
-    MetricsServer,
-    MetricsServerManager,
-    start_metrics_server,
-    get_metrics_server,
-)
+from .metrics import MetricsServer, MetricsServerManager
 from .tracing import start_span, traced
 
 __all__ = [
     "MetricsServer",
     "MetricsServerManager",
-    "start_metrics_server",
-    "get_metrics_server",
     "start_span",
     "traced",
 ]
