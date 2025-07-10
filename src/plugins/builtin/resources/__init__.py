1d252b8c27066d605b3da27c9d66a151cecc6386
Thomas Brittain <honeysucklehardware@gmail.com>
2025-07-10 12:01:09 -0500

    Clean up builtin resources exports

--- src/plugins/builtin/resources/__init__.py
+++ src/plugins/builtin/resources/__init__.py
@@ -1,46 +1,15 @@
 """Resource implementations shipped with the framework."""
 
-from .base import BaseResource, Resource
-from .bedrock import BedrockResource
-from entity.core.resources.container import ResourceContainer
-from .database import DatabaseResource
-from .dsl import ResourceDef, ResourceGraph
-from .duckdb_database import DuckDBDatabaseResource
-from .duckdb_vector_store import DuckDBVectorStore
-from .filesystem import FileSystemResource
+from .echo_llm import EchoLLMResource
 from .llm_base import LLM
 from .llm_resource import LLMResource
-from .postgres import PostgresResource
-from .echo_llm import EchoLLMResource
-from .local_filesystem import LocalFileSystemResource
-from .memory_filesystem import MemoryFileSystem
-from .metrics import MetricsResource
 from .pg_vector_store import PgVectorStore
-from .s3_filesystem import S3FileSystem
-from .structured_logging import StructuredLogging
-from .vector_store import VectorStore, VectorStoreResource
+from .postgres import PostgresResource
 
 __all__ = [
     "LLM",
-    "DatabaseResource",
-    "FileSystemResource",
-    "VectorStore",
-    "VectorStoreResource",
-    "ResourceContainer",
-    "Resource",
-    "BaseResource",
     "LLMResource",
     "EchoLLMResource",
-    "StructuredLogging",
-    "MetricsResource",
-    "DuckDBDatabaseResource",
     "PgVectorStore",
     "PostgresResource",
-    "MemoryFileSystem",
-    "DuckDBVectorStore",
-    "LocalFileSystemResource",
-    "S3FileSystem",
-    "BedrockResource",
-    "ResourceDef",
-    "ResourceGraph",
 ]
