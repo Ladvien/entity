a355b5240b7b82b51282c2136087abbad56d058d
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2025-07-12 10:51:38 -0500

    Removed tests

--- tests/test_standard_resources.py
+++
@@ -1,8 +0,0 @@
-from entity.resources import LLM, Memory, Storage, StandardResources
-
-
-def test_standard_resources_types() -> None:
-    res = StandardResources(memory=Memory({}), llm=LLM({}), storage=Storage({}))
-    assert isinstance(res.memory, Memory)
-    assert isinstance(res.llm, LLM)
-    assert isinstance(res.storage, Storage)
